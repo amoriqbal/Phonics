6f4eef4286ddba9466e2491ac2be20d3f13f2e10
Amor Iqbal <amoriqbalmail@gmail.com>
2025-08-01 02:33:43 +0530

    Landing Page Set

--- Assets/Scripts/DDOL.cs
+++ Assets/Scripts/DDOL.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class DDOL : MonoBehaviour
 {
+    public int mSettingsNumQuestions = 10;
+    public int mSettingsDifficulty = 0;
+    public int mSettingsDelay = 3;
+
     public int mQuestions = 10;
     public int mScore = 0;
     public static DDOL Instance;
--- Assets/Scripts/SceneLoadButtonBehavior.cs
+++ Assets/Scripts/SceneLoadButtonBehavior.cs
@@ -1,36 +1,19 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneLoadButtonBehavior : MonoBehaviour
 {
     public string practiceScenePath;
-    //{
-    //    get
-    //    {
-    //        return practiceScenePath;
-    //    }
-    //    set
-    //    {
-    //        if (SceneManager.GetSceneByPath(value).IsValid())
-    //        {
-    //            practiceScenePath = value;
-    //        }
-    //    }
-    //}
     public string dashboardScenePath;
-    //{
-    //    get
-    //    {
-    //        return dashboardScenePath;
-    //    }
-    //    set
-    //    {
-    //        if (SceneManager.GetSceneByPath(value).IsValid())
-    //        {
-    //            dashboardScenePath = value;
-    //        }
-    //    }
-    //}
+    [SerializeField]
+    private GameObject settingsPanel;
+    [SerializeField]
+    private TMP_InputField textFieldSettingNumQuestions;
+    [SerializeField]
+    private TMP_InputField textFieldSettingDelay;
+    [SerializeField]
+    private TMP_Dropdown dropdownDifficulty;
     public void mOnClickStartButton()
     {
         if(practiceScenePath != null)
@@ -40,6 +23,57 @@ public class SceneLoadButtonBehavior : MonoBehaviour
     public void mOnClickDashboardButton()
     {
         if(practiceScenePath != null)
-            SceneManager.LoadScene(practiceScenePath);
+            SceneManager.LoadScene(dashboardScenePath);
+    }
+
+    public void mOnNumQuestionsFieldChanged()
+    {
+        string xNumQuestionsStr = textFieldSettingNumQuestions.text;
+        if (xNumQuestionsStr != string.Empty)
+        {
+            int lNumQuestions;
+            if (int.TryParse(xNumQuestionsStr, out lNumQuestions))
+            {
+                DDOL.Instance.mSettingsNumQuestions = lNumQuestions;
+            }
+            else
+            {
+                textFieldSettingNumQuestions.text = "10";
+                DDOL.Instance.mSettingsNumQuestions = 10;
+            }
+        }
+    }
+
+    public void mOnDelayFieldChanged()
+    {
+        string xDelayStr = textFieldSettingDelay.text;
+        if (xDelayStr != string.Empty)
+        {
+            int lDelay;
+            if (int.TryParse(xDelayStr, out lDelay))
+            {
+                DDOL.Instance.mSettingsDelay = lDelay;
+            }
+            else
+            {
+                textFieldSettingDelay.text = "3";
+                DDOL.Instance.mSettingsDelay = 3;
+            }
+        }
+    }
+
+    public void mOnDifficultyChanged()
+    {
+        DDOL.Instance.mSettingsDifficulty = dropdownDifficulty.value;
+    }
+    public void mShowHideSettings()
+    {
+        settingsPanel.SetActive(!settingsPanel.activeSelf);
+    }
+    public void Start()
+    {
+        mOnDifficultyChanged();
+        mOnDelayFieldChanged();
+        mOnNumQuestionsFieldChanged();
     }
 }
